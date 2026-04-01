8c45d28897ceb88d6307cb7ecc7a8d0a2c1ca4c3
vignesh magudeeswaran <105164424+vigneshmagudeeswaran@users.noreply.github.com>
2026-04-01 09:25:11 +0530

    Add: products-of-array-discluding-self - submission-7

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-7.py
@@ -0,0 +1,28 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        '''
+        [1,2,4,6]
+    pre=[1,1,2,8]
+    suff=[48,24,6,1]
+    answ=[48,24,12,8]
+        '''
+        pre= [1]* len(nums)
+        suff= [1]* len(nums)
+        res=[1] * len(nums)
+        for i in range(1,len(nums)):
+            # here we need to multiply numbers before that particular number
+            pre[i] = pre[i-1] * nums[i-1]
+            res[i] = pre[i]
+        for i in range(len(nums)-2,-1,-1):
+            suff[i] = suff[i+1] * nums[i+1]
+            res[i] = res[i] *suff[i]
+        
+        # for i in range(len(pre)):
+        #     res[i] = pre[i] * suff[i]
+
+        return res
+
+
+
+
+        
